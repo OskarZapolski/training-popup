af94834b4f675bd61c9d73d2aa835f666ee35298
Oskar Zapolski <oskar.zapolski9@gmail.com>
2023-04-28 16:19:31 +0200

    html i css end

---
+++ index.js
@@ -0,0 +1,7 @@
+const IMGES = document.querySelectorAll("img");
+const PRODUCTS_TEXT = document.querySelectorAll(".text");
+const PRODUCTS = document.querySelectorAll(".product");
+
+PRODUCTS.forEach((product) => {
+  product.addEventListener("click", function () {});
+});
