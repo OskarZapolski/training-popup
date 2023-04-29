6fd367308419653d66034078f4ae73af1a9abb32
Oskar Zapolski <oskar.zapolski9@gmail.com>
2023-04-29 15:00:37 +0200

    wiekszosc juz

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const IMGES = document.querySelectorAll("img");
 const PRODUCTS_TEXT = document.querySelectorAll(".text");
 const PRODUCTS = document.querySelectorAll(".product");
+const BLUR = document.querySelector(".blur");
+const POPUP = document.querySelector(".popup");
+const IMG_POPUP = document.querySelector(".img-popup");
 
-PRODUCTS.forEach((product) => {
-  product.addEventListener("click", function () {});
+function ShowPopup() {
+  BLUR.classList.remove("hid");
+  POPUP.classList.remove("hid");
+}
+function HidPopup() {
+  BLUR.classList.add("hid");
+  POPUP.classList.add("hid");
+}
+
+PRODUCTS.forEach((product, index) => {
+  product.addEventListener("click", function (e) {
+    POPUP.innerHTML = `
+    <img src="${IMGES[index].src}" alt="" class="img-popup" /><ul class="popup-list"><li class="text">${PRODUCTS_TEXT[index].innerText}</li><li class="text">${PRODUCTS_TEXT[index].innerText}</li><li class="text">${PRODUCTS_TEXT[index].innerText}</li></ul>`;
+    ShowPopup();
+  });
+});
+
+BLUR.addEventListener("click", function (e) {
+  console.log(e);
 });
